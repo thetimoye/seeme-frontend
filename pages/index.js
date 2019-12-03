6d66e7eb7e318090ce1f23be8b35d8ff12962fcf
thetimoye <timmine7@gmail.com>
2019-12-03 07:39:59 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class IndexPage extends Component{
+    state = {
+        hi: ""
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Say hi to the index page</p>
+            </div>
+        )
+    }
+}
+
+export default IndexPage
